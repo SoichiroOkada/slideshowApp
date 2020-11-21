2df0ade99a34522e8d1715887f209adaecab672f
soichiro.okada <straycat0914@gmail.com>
2020-11-21 13:10:55 +0900

    途中2

--- slideshowApp/ViewController.swift
+++ slideshowApp/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func idou(_ sender: Any) {_ segue: UIStoryboardSegue(tabview)
+    }
+    
+    @IBOutlet weak var imageview: UIImageView!
+    @IBAction func back(_ segue: UIStoryboardSegue){}
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let img01 = UIImage(named: "bed")
+        imageview.image = img01
+        
+      //  let img01 = UIImageView(image: UIImage(named: "bed"))
+      //  let img02 = UIImageView(image: UIImage(named: "chair"))
+      //  let img03 = UIImageView(image: UIImage(named: "box"))
+      //  let img04 = UIImageView(image: UIImage(named: "nail"))
+      //  img04.frame = CGRect(x: 200, y: 200, width: 200, height: 200)
+      //  self.view.addSubview(img04)
     }
 
 
---
+++ slideshowApp/tabview.swift
@@ -0,0 +1,13 @@
+//
+//  tabview.swift
+//  slideshowApp
+//
+//  Created by straycat on 2020/11/21.
+//  Copyright © 2020 soichiro.okada. All rights reserved.
+//
+
+import UIKit
+
+class tabview: NSObject {
+
+}
